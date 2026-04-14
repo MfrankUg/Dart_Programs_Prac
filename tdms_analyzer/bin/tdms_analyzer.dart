66b2ecb7ff19abfffb0805bf0d0250331e037e62
MfrankUg <muhindofrank2001@gmail.com>
2026-04-14 20:37:14 +0300

    practice on maps

--- tdms_analyzer/bin/tdms_analyzer.dart
+++ tdms_analyzer/bin/tdms_analyzer.dart
@@ -1,5 +1,6 @@
 import 'package:tdms_analyzer/tdms_analyzer.dart' as tdms_analyzer;
-// finding the maximum values
+
+// finding the maximum values using for loops
 void main(List<String> arguments) {
   List<double> temperatures = [26.5, 29.1, 27.8, 30.2, 28.5];
   List<double> humidityLevels = [-1, -2, -4, -0.5];
@@ -20,5 +21,16 @@ void main(List<String> arguments) {
 
   print("ALERT: The hottest zone in the warehouse reached $maxTemp°C!");
   print("ALERT: The least dump zone in the warehouse reached $maxHumidity %!");
+
+  prac();
 }
 
+// maps
+void prac() {
+  Map<String, double> warehouseZones = {
+    'North Wing' : 26.5,
+    'South Wing' : 29.1,
+    'Loading Wing':  30.2,
+    'Strong A' : 27.8
+  };
+}
