fdea8e5cae5f32b6ae79060c2147cb95eaf0a82f
MfrankUg <muhindofrank2001@gmail.com>
2026-04-14 19:58:58 +0300

    more on for loop executable prac

--- tdms_analyzer/bin/tdms_analyzer.dart
+++ tdms_analyzer/bin/tdms_analyzer.dart
@@ -1,13 +1,13 @@
 import 'package:tdms_analyzer/tdms_analyzer.dart' as tdms_analyzer;
 
 void main(List<String> arguments) {
-  List<int> dailyDustReadings = [300, 450, 600, 200, 550, 480, 700];
-  int dangerCount = 0;
-  for (int readings in dailyDustReadings) {
-    if (readings > 500) {
-      dangerCount++;
+  List<double> temperatures = [26.5, 29.1, 27.8, 30.2, 28.5];
+  double maxTemp = 0.0;
+  for (double currentTemp in temperatures) {
+    if (currentTemp > maxTemp) {
+      maxTemp = currentTemp;
     }
-    
   }
-  print("Daily Report: The coffee warehouse experienced $dangerCount dangerous dust spikes today.");
+
+  print("ALERT: The hottest zone in the warehouse reached $maxTemp°C!");
 }
