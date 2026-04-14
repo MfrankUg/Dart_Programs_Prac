4949193bc071d24fa7c2a3f87450c218f01f10f2
MfrankUg <muhindofrank2001@gmail.com>
2026-04-14 20:16:30 +0300

    more on finding the maximum value in a list using a for loop

--- tdms_analyzer/bin/tdms_analyzer.dart
+++ tdms_analyzer/bin/tdms_analyzer.dart
@@ -2,6 +2,7 @@ import 'package:tdms_analyzer/tdms_analyzer.dart' as tdms_analyzer;
 
 void main(List<String> arguments) {
   List<double> temperatures = [26.5, 29.1, 27.8, 30.2, 28.5];
+  List<double> humidityLevels = [-1, -2, -4, -0.5];
   double maxTemp = 0.0;
   for (double currentTemp in temperatures) {
     if (currentTemp > maxTemp) {
@@ -9,5 +10,15 @@ void main(List<String> arguments) {
     }
   }
 
+  double maxHumidity = humidityLevels[0];
+
+  for (double Humid in humidityLevels) {
+    if (maxHumidity > Humid) {
+      maxHumidity = Humid;
+    }
+  }
+
   print("ALERT: The hottest zone in the warehouse reached $maxTemp°C!");
+  print("ALERT: The least dump zone in the warehouse reached $maxHumidity %!");
 }
+
