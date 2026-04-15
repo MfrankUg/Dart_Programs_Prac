3fdf3054f7fb39a92b6927c063bfbc91a90b222f
MfrankUg <muhindofrank2001@gmail.com>
2026-04-15 20:45:10 +0300

    tdms trial on classes

---
+++ tdms_analyzer_test1/bin/tdms_analyzer_test1.dart
@@ -0,0 +1,5 @@
+import 'package:tdms_analyzer_test1/tdms_analyzer_test1.dart' as tdms_analyzer_test1;
+
+void main(List<String> arguments) {
+  print('Hello world: ${tdms_analyzer_test1.calculate()}!');
+}
---
+++ tdms_analyzer_test1/test/tdms_analyzer_test1_test.dart
@@ -0,0 +1,8 @@
+import 'package:tdms_analyzer_test1/tdms_analyzer_test1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
