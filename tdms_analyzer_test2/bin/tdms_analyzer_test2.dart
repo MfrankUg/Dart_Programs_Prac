57a2b2883592d9695b683892a6a81b9931799475
MfrankUg <muhindofrank2001@gmail.com>
2026-04-16 21:04:32 +0300

    new task

---
+++ tdms_analyzer_test2/bin/tdms_analyzer_test2.dart
@@ -0,0 +1,7 @@
+import 'package:tdms_analyzer_test2/tdms_analyzer_test2.dart' as tdms_analyzer_test2;
+
+
+
+void main(List<String> arguments) {
+  print('Hello world: ${tdms_analyzer_test2.calculate()}!');
+}
---
+++ tdms_analyzer_test2/test/tdms_analyzer_test2_test.dart
@@ -0,0 +1,8 @@
+import 'package:tdms_analyzer_test2/tdms_analyzer_test2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
